d0fa322218b9690b519742acae5ba5f1624c6e2e
Alex Page <alex@alexpage.com.au>
2021-04-25 17:24:28 -0700

    0.1.0-alpha.13

--- npm/get-binary.js
+++ npm/get-binary.js
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
 const os = require("os");
-const stream = require("stream");
-const { promisify } = require("util");
 const fs = require("fs");
-const got = require("got");
-const tar = require("tar");
-const path = require("path");
-const childProcess = require("child_process");
+const {join} = require("path");
+const {spawnSync} = require("child_process");
+const {get} = require('https');
+const {unzipSync, gzip} = require('zlib');
 
-const pipeline = promisify(stream.pipeline);
-
-const { version, name } = require("../package.json");
-const repositoryUrl = "https://github.com/shopify/get-repo-images";
-const rimraf = require("rimraf");
+const { version, repository, name } = require("../package.json");
 
 const platformFiles = {
   "darwin x64": "darwin-amd64",
@@ -23,33 +17,83 @@ const platformFiles = {
 };
 const platformKey = `${process.platform} ${os.arch()}`;
 const platform = platformFiles[platformKey];
-const binDir = path.join(__dirname, "bin");
-const binaryFile = path.join(binDir, name);
+const binDir = join(__dirname, "bin");
+const binaryFile = join(binDir, name);
 
 if (!platform) {
   new Error(`Unsupported platform ${platformKey}`);
   process.exit(1);
 }
 
+const removeRecursive = (dir) => {
+  for (const entry of fs.readdirSync(dir)) {
+    const entryPath = join(dir, entry);
+    let stats;
+    try {
+      stats = fs.lstatSync(entryPath);
+    } catch (e) {
+      continue; // Guard against https://github.com/nodejs/node/issues/4760
+    }
+    if (stats.isDirectory()) removeRecursive(entryPath);
+    else fs.unlinkSync(entryPath);
+  }
+  fs.rmdirSync(dir);
+}
+
+const fetch = (url) => {
+  return new Promise((resolve, reject) => {
+    get(url, res => {
+      if ((res.statusCode === 301 || res.statusCode === 302) && res.headers.location)
+        return fetch(res.headers.location).then(resolve, reject);
+      if (res.statusCode !== 200)
+        return reject(new Error(`Server responded with ${res.statusCode}`));
+      let chunks = [];
+      res.on('data', chunk => chunks.push(chunk));
+      res.on('end', () => resolve(Buffer.concat(chunks)));
+    }).on('error', reject);
+  });
+}
+
+const extractFile = (buffer, file) => {
+  try {
+    buffer = unzipSync(buffer);
+  } catch (err) {
+    throw new Error(`Invalid gzip data in archive: ${err && err.message || err}`);
+  }
+
+  let str = (i, n) => String.fromCharCode(...buffer.subarray(i, i + n)).replace(/\0.*$/, '');
+  let offset = 0;
+  while (offset < buffer.length) {
+    let name = str(offset, 100);
+    let size = parseInt(str(offset + 124, 12), 8);
+    offset += 512;
+    if (!isNaN(size)) {
+      if (name === file) return buffer.subarray(offset, offset + size);
+      offset += (size + 511) & ~511;
+    }
+  }
+  throw new Error(`Could not find ${file} in archive`);
+}
+
 const install = async () => {
   try {
     if (fs.existsSync(binDir)) {
-      rimraf.sync(binDir);
+      removeRecursive(binDir);
     }
     fs.mkdirSync(binDir, { recursive: true });
-    const url = `${repositoryUrl}/releases/download/v${version}/${platform}.tar.gz`;
-    console.log("Downloading binary: ", url);
-    await pipeline(got.stream(url), tar.x({ C: binDir }));
+    const url = `${repository.url}/releases/download/v${version}/${platform}.tar.gz`;
+    const fileBuffer = await fetch(url);
+    const gzipFile = extractFile(fileBuffer, name);
+    fs.writeFileSync(binaryFile, gzipFile, { mode: 0o755 });
   } catch (error) {
-    console.error(error);
-    process.exit(1);
+    throw new Error(error);
   }
 };
 
 const run = () => {
   const [, , ...args] = process.argv;
   const options = { cwd: process.cwd(), stdio: "inherit" };
-  const { error, status } = childProcess.spawnSync(binaryFile, args, options);
+  const { error, status } = spawnSync(binaryFile, args, options);
 
   if (error) {
     console.error(error);
@@ -60,7 +104,7 @@ const run = () => {
 };
 
 const uninstall = () => {
-  rimraf.sync(binDir);
+  removeRecursive(binDir);
 };
 
 module.exports = {
