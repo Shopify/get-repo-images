a78e266666b61fc7cfff9202b22ef91c5eceb6cb
Alex Page <alex@alexpage.com.au>
2021-05-03 20:12:36 -0700

    Improve SEO and accessibility (#17)

--- site-template/src/pages/index.jsx
+++ site-template/src/pages/index.jsx
@@ -62,46 +62,73 @@ function HomePage() {
     setRepo(value);
   }
 
+  const customStyles = {
+    placeholder: () => ({
+      color: '#6F7676'
+    }),
+    control: (provided) => ({
+      ...provided,
+      border: '1px solid #c9cccf',
+      boxShadow: 'rgba(0, 0, 0, 0.05) 0px 1px 0px 0px'
+    })
+  }
+
   return (
     <>
       <Head>
         <link rel="icon" type="image/png" href="/favicon.png" />
         <title>@shopify/get-repo-images</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Browse, sort and filter images found with @shopify/get-repo-images" />
       </Head>
       <header>
         <Image src="/logo.svg" width="32" height="32" alt="Shopify logo" />
         <div className="search">
           <div className="icon">
-            <svg viewBox="0 0 20 20" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg"><path d="M2 8c0-3.309 2.691-6 6-6s6 2.691 6 6-2.691 6-6 6-6-2.691-6-6zm17.707 10.293l-5.395-5.396A7.946 7.946 0 0016 8c0-4.411-3.589-8-8-8S0 3.589 0 8s3.589 8 8 8a7.954 7.954 0 004.897-1.688l5.396 5.395A.998.998 0 0020 19a1 1 0 00-.293-.707z" fill="currentColor"/></svg>
+            <svg viewBox="0 0 20 20" width="16px" height="16px" xmlns="http://www.w3.org/2000/svg"><path d="M2 8c0-3.309 2.691-6 6-6s6 2.691 6 6-2.691 6-6 6-6-2.691-6-6zm17.707 10.293l-5.395-5.396A7.946 7.946 0 0016 8c0-4.411-3.589-8-8-8S0 3.589 0 8s3.589 8 8 8a7.954 7.954 0 004.897-1.688l5.396 5.395A.998.998 0 0020 19a1 1 0 00-.293-.707z" fill="currentColor"/></svg>
           </div>
+          <label htmlFor="search-input" className="sr-only">Search</label>
           <input
+            id="search-input"
             type="text"
             value={search}
             placeholder={`Search ${totalImages} images`}
             onChange={handleSearch}
           />
         </div>
-        <Select
-          name="Repo"
-          instanceId="repo-select"
-          placeholder="Repository"
-          isMulti
-          options={tags}
-          onChange={handleRepo}
-        />
-        <Select
-          name="Sort"
-          placeholder="Sort"
-          instanceId="sort-select"
-          onChange={setSort}
-          defaultValue={sortItems[0]}
-          options={sortItems}
-        />
+        <div>
+          <label htmlFor="repo-input" className="sr-only">Repository</label>
+          <Select
+            inputId="repo-input"
+            name="Repo"
+            instanceId="repo-select"
+            placeholder="Repository"
+            isMulti
+            options={tags}
+            onChange={handleRepo}
+            styles={customStyles}
+          />
+        </div>
+        <div>
+          <label htmlFor="sort-input" className="sr-only">Sort</label>
+          <Select
+            inputId="sort-input"
+            name="Sort"
+            placeholder="Sort"
+            instanceId="sort-select"
+            onChange={setSort}
+            defaultValue={sortItems[0]}
+            options={sortItems}
+            styles={customStyles}
+          />
+        </div>
       </header>
       <main>
         <ul className="cards">
           {images.map(image =>
-            <li key={image.path}><ImageCard image={image} referenceOnClick={setUsageModal} /></li>
+            <li key={image.path}>
+              <ImageCard image={image} referenceOnClick={setUsageModal} />
+            </li>
           )}
         </ul>
       </main>
