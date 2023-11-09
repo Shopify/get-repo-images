65dc52c394c6194791fb1e05abb1f3eb1e78d007
Alex Page <alex.page@shopify.com>
2023-11-09 14:03:26 +1100

    Support arm64

--- npm/exec.js
+++ npm/exec.js
@@ -3,18 +3,19 @@ const os = require("os");
 const fs = require("fs");
 const stream = require("stream");
 const childProcess = require("child_process");
-const path = require('path');
+const path = require("path");
 
 const got = require("got");
 const tar = require("tar");
 
-const {name, version} = require("../package.json");
-const binaryFile = name.split('/')[1];
-const packageDir =  path.join(__dirname, '..')
+const { name, version } = require("../package.json");
+const binaryFile = name.split("/")[1];
+const packageDir = path.join(__dirname, "..");
 const binaryLocation = path.join(packageDir, binaryFile);
 
 const platformFiles = {
   "darwin x64": "darwin-amd64",
+  "darwin arm64": "darwin-arm64",
   "linux x64": "linux-amd64",
   "linux 386": "linux-386",
   "win32 x64": "windows-amd64",
@@ -38,16 +39,16 @@ const install = () => {
   stream.pipeline(
     got.stream(url),
     tar.x({ C: packageDir }),
-    err => err && console.error(err.message)
+    (err) => err && console.error(err.message)
   );
 };
 
 const run = () => {
   const [, , ...args] = process.argv;
-  args.push('-nodedir', process.cwd());
+  args.push("-nodedir", process.cwd());
   const result = childProcess.spawnSync(binaryLocation, args, {
     stdio: "inherit",
-    cwd: packageDir
+    cwd: packageDir,
   });
 
   if (result.error) {
