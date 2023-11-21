cb90bada8d0d39130dd0b6073e1c475b12012782
Alex Page <alex.page@shopify.com>
2023-11-21 20:01:06 +1100

    Only add auth if token exists

--- cmd/get-repo-images/clone.go
+++ cmd/get-repo-images/clone.go
@@ -14,16 +14,21 @@ func clone(repo string, tmpDir string, token string) error {
 		url = "https://" + token + "@github.com/" + repo + ".git"
 	}
 
-	_, err := git.PlainClone(tmpDir, false, &git.CloneOptions{
-		Auth: &http.BasicAuth{
-			Username: "abc123",
-			Password: token,
-		},
+	cloneOpts := &git.CloneOptions{
 		URL:           url,
 		SingleBranch:  true,
 		Depth:         1,
 		Progress:      os.Stdout,
-	})
+	}
+
+	if token != "" {
+		cloneOpts.Auth = &http.BasicAuth{
+			Username: "abc123",
+			Password: token,
+		}
+	}
+
+	_, err := git.PlainClone(tmpDir, false, cloneOpts)
 
 	if err != nil {
 		fmt.Println(repo, "failed to clone or authenticate")
