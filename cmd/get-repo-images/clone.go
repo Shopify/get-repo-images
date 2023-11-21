43617ef270745bed2c9edec07a701acd6df60964
Alex Page <alex.page@shopify.com>
2023-11-21 19:33:41 +1100

    Simplify clone

--- cmd/get-repo-images/clone.go
+++ cmd/get-repo-images/clone.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"os"
 
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/transport"
-	"github.com/go-git/go-git/v5/plumbing/transport/client"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 func clone(repo string, tmpDir string, token string) error {
@@ -15,44 +14,19 @@ func clone(repo string, tmpDir string, token string) error {
 		url = "https://" + token + "@github.com/" + repo + ".git"
 	}
 
-	// Get the HEAD reference for non master branches
-	// https://github.com/go-git/go-git/issues/249#issuecomment-772354474
-	e, err := transport.NewEndpoint(url)
-	if err != nil {
-		return err
-	}
-
-	cli, err := client.NewClient(e)
-	if err != nil {
-		return err
-	}
-
-	s, err := cli.NewUploadPackSession(e, nil)
-	if err != nil {
-		return err
-	}
-
-	info, err := s.AdvertisedReferences()
-	if err != nil {
-		return errors.New("Could not find or authenticate repo " + repo)
-	}
-
-	refs, err := info.AllReferences()
-	if err != nil {
-		return err
-	}
-
-	headReference := refs["HEAD"].Target()
-
-	_, err = git.PlainClone(tmpDir, false, &git.CloneOptions{
+	_, err := git.PlainClone(tmpDir, false, &git.CloneOptions{
+		Auth: &http.BasicAuth{
+			Username: "abc123",
+			Password: token,
+		},
 		URL:           url,
 		SingleBranch:  true,
 		Depth:         1,
-		ReferenceName: headReference,
+		Progress:      os.Stdout,
 	})
 
 	if err != nil {
-		fmt.Println(repo, "failed to clone")
+		fmt.Println(repo, "failed to clone or authenticate")
 		return err
 	}
 
